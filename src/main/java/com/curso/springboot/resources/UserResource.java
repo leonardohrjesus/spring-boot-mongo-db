0e46f4a72a7f1d215c39c325df48dca12b37ae58
Leonardo <leonardo.hrjesus@hotmail.com>
2020-05-21 17:34:22 -0300

    Obtendo usuario por id

--- src/main/java/com/curso/springboot/resources/UserResource.java
+++ src/main/java/com/curso/springboot/resources/UserResource.java
@@ -5,13 +5,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.springboot.domain.User;
 import com.curso.springboot.dto.UserDTO;
-import com.curso.springboot.service.UserService;
+import com.curso.springboot.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
@@ -26,6 +27,15 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 				
 		return ResponseEntity.ok().body(listDto);
+		
+		
+	}
+	
+	@RequestMapping(value="/{id}", method= RequestMethod.GET)
+	public ResponseEntity <UserDTO>  findById(@PathVariable String id ){
+		User obj = service.findById(id);
+				
+		return ResponseEntity.ok().body(new UserDTO(obj));
 		
 		
 	}
--- src/main/java/com/curso/springboot/services/UserService.java
+++ src/main/java/com/curso/springboot/services/UserService.java
@@ -1,21 +1,28 @@
-package com.curso.springboot.service;
+package com.curso.springboot.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.curso.springboot.domain.User;
 import com.curso.springboot.respository.UserRepository;
+import com.curso.springboot.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public  List<User> findAll() {
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
