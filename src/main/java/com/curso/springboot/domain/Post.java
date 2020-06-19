017fb24e2651e1a4564718179c5610701d2e1c84
Leonardo <leonardo.hrjesus@hotmail.com>
2020-06-18 21:34:51 -0300

    Consulta simple com query methods

--- src/main/java/com/curso/springboot/domain/Post.java
+++ src/main/java/com/curso/springboot/domain/Post.java
@@ -1,12 +1,15 @@
 package com.curso.springboot.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.curso.springboot.dto.AuthorDTO;
+import com.curso.springboot.dto.CommentDTO;
 
 @Document
 public class Post  implements Serializable{
@@ -19,6 +22,8 @@ public class Post  implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author; 
+	
+	private List<CommentDTO> comments = new ArrayList<>(); 	
 	
 	public Post() {
 		
@@ -84,6 +89,14 @@ public class Post  implements Serializable{
 		return result;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -100,6 +113,7 @@ public class Post  implements Serializable{
 			return false;
 		return true;
 	}
+
 
 	
 	
