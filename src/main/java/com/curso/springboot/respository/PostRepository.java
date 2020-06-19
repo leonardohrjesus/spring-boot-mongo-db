01bd407fcbba9a952f34a61d10c277567ee83650
Leonardo <leonardo.hrjesus@hotmail.com>
2020-06-18 21:54:40 -0300

    Consulta simple com query methods

--- src/main/java/com/curso/springboot/respository/PostRepository.java
+++ src/main/java/com/curso/springboot/respository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.curso.springboot.respository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.curso.springboot.domain.Post;
 
 @Repository
 public interface PostRepository  extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text); 
 
 	
 }
